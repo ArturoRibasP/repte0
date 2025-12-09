6b04544565529b73cf5d487c8d54b7ebb2fcba1e
= <=>
2025-12-09 08:42:52 +0100

    cambios2 9/12/25

--- UT06/md6_1/script.js
+++ UT06/md6_1/script.js
@@ -1,2 +1,11 @@
+//--- Exemple 1 ---
+console.log("----- Exemple 1 -----");
+let llista1 = [1, 2, 3, 4];
+let llista2 = ["Joan", "Marc", "Robert"];
+let llista3 = ["192.168.0.10", "Mordor", ["8.8.8.8", "8.8.4.4"]];
+
+
+//--- Exemple 2 ---
+console.log("----- Exemple 2 -----");
 let algunsNombresNaturals = [101, 102, 103, 104, 105];
-console.log(algunsNombresNaturals[4]);
+console.log(algunsNombresNaturals[2]);
--- UT06/md6_2/script.js
+++ UT06/md6_2/script.js
@@ -1,1 +1,34 @@
-alert("neck hurt")
+// Variables
+let llistaNombres = [1, 2, 3, 4, 5];
+let llistaMixta = ["Blanca", "Mateu", 33, true];
+
+
+// Execució
+console.log(typeof llistaNombres);     // "object" → és un array
+console.log(typeof llistaMixta[1]);    // "string"
+console.log(typeof llistaMixta[2]);    // "number"
+console.log(typeof llistaMixta[3]);    // "boolean"
+
+
+// Comprovació més exacta
+console.log(Array.isArray(llistaNombres)); // true
+
+//--- Exemple 1 ---
+// Variables
+let abecedari = ["a", "b", ["C", "D"], "e", "f"];
+// Execució
+console.log(abecedari[2]);
+
+//--- Exemple 2 ---
+// Variables
+let nombres = [1200, [2200, 3200, 4200], 5200, 6200];
+// Execució
+console.log(nombres[1][nombres[1].length - 1]); // Mètode tradicional
+console.log(nombres[1].at(-1));                 // Mètode modern
+
+//--- Exemple 3 ---
+// Variables
+let arbres = ["taronjer", "perera", "pomera", ["ametller", "garrofer"]];
+// Execució
+console.log(arbres[1][2]);  // Extreu la lletra "r" de "perera"
+
